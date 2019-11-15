90b247db4a64dfa36685d5e42a9a77f9f20aa49c
Carston Henderson <carstonhenderson@icloud.com>
2019-11-15 15:21:00 -0700

    Fix height on mobile screens

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,11 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './layout.css'
 
-const Layout = ({ children }) => (
-  <main className="bg-gray-900 h-screen text-gray-100 flex flex-col justify-center items-center">
-    {children}
-  </main>
-)
+const Layout = ({ children }) => {
+  const [height, setHeight] = useState('100vh')
+
+  useEffect(() => {
+    setHeight(window.innerHeight)
+  })
+
+  return (
+    <main
+      className="bg-gray-900 text-gray-100 flex flex-col justify-center items-center"
+      style={{ height: height }}
+    >
+      {children}
+    </main>
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
