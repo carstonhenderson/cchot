f13a229da4f9828c8cbb696d376612db8f02c742
Carston Henderson <carstonhenderson@icloud.com>
2020-01-11 13:16:02 -0700

    Change text depending on if date selected is today or not

--- src/components/dateResponse.js
+++ src/components/dateResponse.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import moment from 'moment'
 
-const dateResponse = ({ available }) => (
+const dateResponse = ({ date, available }) => (
   <>
-    <h1 className="text-xl">Can Carston hang out today?</h1>
+    <h1 className="text-xl">
+      Can Carston hang out{' '}
+      {date.isSame(moment(), 'day')
+        ? 'today'
+        : `on ${date.format('dddd, MMMM Do')}`}
+      ?
+    </h1>
     <h2
       className={`text-6xl font-bold ${
         available ? 'text-green-500' : 'text-red-500'
